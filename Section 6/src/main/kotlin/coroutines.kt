b7520f39ae84bab20821703ad1a05cdb0896f913
Stepan Goncharov <step.89.g@gmail.com>
2018-03-06 23:03:22 +0800

    Example for 6.2 video

--- Section 6/src/main/kotlin/coroutines.kt
+++ Section 6/src/main/kotlin/coroutines.kt
@@ -1,18 +1,27 @@
 import arrow.core.Either
 import arrow.effects.IO
+import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import kotlin.coroutines.experimental.suspendCoroutine
+//Coroutine dispatcher
+import kotlinx.coroutines.experimental.javafx.JavaFx as UI
 
 suspend fun <T : Any> IO<T>.await(): Either<Throwable, T> =
         suspendCoroutine { continuation ->
             unsafeRunAsync(continuation::resume)
         }
 
-fun main(args: Array<String>) = runBlocking {
-    IO { "Hello Coroutine" }
-            .await()
-            .fold(
-                    { println("Error $it")},
-                    { println("Success $it")}
-            )
+fun main(args: Array<String>) {
+    runBlocking {
+        IO { "Hello Coroutine" }
+                // Run asynchronously
+                .await()
+                // Wait for result on Coroutine dispatcher
+                .fold(
+                        { println("Error $it")},
+                        { println("Success $it")}
+                )
+    }
+
+    launch(UI) {  }
 }
